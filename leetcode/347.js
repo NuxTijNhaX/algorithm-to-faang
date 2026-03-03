cddce9af841798665abd83a1fc54043651ea454e
DevNhaX <nhanguyen7901@gmail.com>
2026-03-03 14:06:41 +0700

    update

---
+++ leetcode/347.js
@@ -0,0 +1,24 @@
+function topKFrequent(nums, k) {
+  const count = {};
+  const freq = Array.from({ length: nums.length + 1 }, () => []);
+
+  for (const num of nums) {
+    count[num] = (count[num] || 0) + 1;
+  }
+
+  for (const n in count) {
+    freq[count[n]].push(parseInt(n));
+  }
+
+  const result = [];
+
+  for (let index = freq.length - 1; index > 0; index--) {
+    for (const element of freq[index]) {
+      result.push(element);
+
+      if (result.length === k) return result;
+    }
+  }
+
+  return result;
+}
